b1e2c203a8b7ab7c4a28b1d45320c7e6d6d97669
Constantine Nicolaou <cn5@sanger.ac.uk>
2010-06-28 10:01:01 +0300

    Added ruby unit test

--- test/morning_glory_test.rb
+++ test/morning_glory_test.rb
@@ -11,16 +11,8 @@ CONTENT_TYPES   = {
 REGEX_ROOT_RELATIVE_CSS_URL = /url\((\'|\")?(\/+.*(#{CONTENT_TYPES.keys.map { |k| '\.' + k.to_s }.join('|')}))\1?\).*/
 REPLACEMENT = "url(\1#{RAILS_ASSET_ID}\2\1)"
 
-class MorningGloryTest < ActiveSupport::TestCase
-  test "CSS URL replacement" do
-    assert_equals "background: url(/images/test.png)".gsub(REGEX_ROOT_RELATIVE_CSS_URL, REPLACEMENT), ""
-  end
-  
-  test "CSS URL replacement - nested folder" do
-    assert_equals "background: url(/images/test.png)".gsub(REGEX_ROOT_RELATIVE_CSS_URL, REPLACEMENT), ""
-  end
-  
-  test "CSS URL replacment" do
-    assert_equals "background: url(/images/test.png)".gsub(REGEX_ROOT_RELATIVE_CSS_URL, REPLACEMENT), ""
+class MorningGloryTest < Test::Unit::TestCase
+  def test_replace_url
+    assert_equal("background: url(\001REV_TEST\002\001)", "background: url(/images/test.png)".gsub(REGEX_ROOT_RELATIVE_CSS_URL, REPLACEMENT))
   end
 end
